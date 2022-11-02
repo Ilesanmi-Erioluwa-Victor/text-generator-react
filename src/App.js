f2d603d2e1bc3ab5143448f249be51b5509d903b
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-02 09:38:33 +0100

    Added my handler

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
+  const HandleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
   return (
     <section className="section-center">
       <h3>Tired of the same Lorem Text ?</h3>
+
+      <form className="lorem-form" onSubmit={() => HandleSubmit}></form>
     </section>
   );
 }
