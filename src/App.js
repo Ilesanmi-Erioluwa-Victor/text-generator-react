da527e29b686d548b9452fe0c4671f24df06b0d9
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-02 09:41:17 +0100

    Added my inputs and label

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ function App() {
     <section className="section-center">
       <h3>Tired of the same Lorem Text ?</h3>
 
-      <form className="lorem-form" onSubmit={() => HandleSubmit}></form>
+      <form className="lorem-form" onSubmit={HandleSubmit}>
+        <label htmlFor="amount">Paragraphs</label>
+        <input name="amount" id="amount" type={"number"} />
+      </form>
     </section>
   );
 }
