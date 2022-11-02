d692c33f88097e62a3313d9404a55cd818719d0e
Erioluwa Ilesanmi <ilesanmierioluwavictor@gmail.com>
2022-11-02 09:35:26 +0100

    Started working on my form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
-  return <section className="section-center"></section>;
+  return (
+    <section className="section-center">
+      <h3>Tired of the same Lorem Text ?</h3>
+    </section>
+  );
 }
 
 export default App;
